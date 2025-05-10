16ef5ed8483786f73375726e7ab0d20e5e945d78
soumyakantamitra <soumyakantamitra89@gmail.com>
2025-05-10 13:21:04 +0530

    added loading screen

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -6,6 +6,8 @@ export default function Content({ changeScore }) {
 
   const [clicked, setClicked] = useState(new Set([]));
 
+  const [loading, setLoading] = useState(true);
+
   function checkScore(currentCardClick) {
     if (clicked.has(currentCardClick)) {
       console.log("exists") 
@@ -37,10 +39,32 @@ export default function Content({ changeScore }) {
         promises.push({id, name, image : `https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${id}.svg`});
       }
       setPokemons(promises);
+      setLoading(false);
     }
 
     fetchPokemons();
   }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+    viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
+          <path fill="#fff" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
+          <animateTransform 
+            attributeName="transform" 
+            attributeType="XML" 
+            type="rotate"
+            dur="1s" 
+            from="0 50 50"
+            to="360 50 50" 
+            repeatCount="indefinite" />
+          </path>
+        </svg>
+      </main>
+    );
+  }
+
   return (
     <main>
       {pokemons.map((data) => {
